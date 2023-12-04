ab938732e9d27ee1fd8582d847f8756fffb25b38
stanley-world <146215090+stanley-world@users.noreply.github.com>
2023-12-04 16:58:32 +0000

    rail

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  
+  get("/square/new",{:controller =>"game", :action => "user_played_rock"})
+  get("/payment",{:controller =>"game", :action => "user_played_paper"}) 
+
 end
